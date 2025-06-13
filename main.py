8cbc7e7e94d1536e9480eef2b17a4e0412b9d9bb
Hellen Aldenia Rovi <124753028+Hellen240403@users.noreply.github.com>
2025-06-13 09:55:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-import home as home, eda, forecast, prediction, about
+import home as home, forecast, prediction, about
 
 st.set_page_config(
     page_title="Dashboard"

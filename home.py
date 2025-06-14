335918de7a6a4ff2c672e8656ef5d194dc4cc202
Hellen Aldenia Rovi <124753028+Hellen240403@users.noreply.github.com>
2025-06-15 01:51:48 +0700

    Update home.py

--- home.py
+++ home.py
@@ -65,23 +65,24 @@ def app():
 
     with col2:
         st.markdown("""
-        <div class='weather-box' style="
-            background-color: 0;
-            border-radius: 15px;
+        <div style="
+            background-color: rgba(255, 255, 255, 0.6);
+            border-radius: 18px;
             padding: 20px;
-            box-shadow: 0;
-            margin-bottom: 10px;
+            margin-bottom: 5px;
+            width: fit-content;
         ">
             <ul style="list-style: none; padding-left: 0; margin: 0; font-size: 16px;">
-                <li>🌡️ <b>Suhu:</b> 29°C</li>
-                <li>💧 <b>Kelembapan:</b> 78%</li>
-                <li>🌬️ <b>Angin:</b> 26 km/h</li>
-                <li>🌞 <b>UV:</b> Ekstrem</li>
+                <li>🌡️ <span style="font-weight:bold; color:#d32f2f;">Suhu:</span> 29°C</li>
+                <li>💧 <span style="font-weight:bold; color:#0288d1;">Kelembapan:</span> 78%</li>
+                <li>🌬️ <span style="font-weight:bold; color:#0277bd;">Angin:</span> 26 km/h</li>
+                <li>🌞 <span style="font-weight:bold; color:#fbc02d;">UV:</span> Ekstrem</li>
             </ul>
         </div>
         """, unsafe_allow_html=True)
         
         st.caption("📌 Data dari BMKG, diolah kembali oleh sistem prediksi cuaca.")
+
 
     with st.expander("📘 Pendahuluan", expanded=True):
         tab1, tab2, tab3 = st.tabs(["📖 Latar Belakang", "🎯 Tujuan", "🎁 Manfaat"])

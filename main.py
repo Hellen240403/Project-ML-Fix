4508c77916a3f1144e00dae997fa70aef9cfa6e4
Hellen Aldenia Rovi <124753028+Hellen240403@users.noreply.github.com>
2025-06-14 22:48:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class MultiApp:
                     "container": {"padding": "5!important", "background-color": '#081f5c'},
                     "icon": {"color": "white", "font-size": "23px"},
                     "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "#a7ebf2"},
-                    "nav-link-selected": {"background-color": "#02ab21"},
+                    "nav-link-selected": {"background-color": "black"},
                 }
             )
 

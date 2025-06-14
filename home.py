f7a7bd6b975a88eddb219dcc891384dc17e0fee6
Hellen Aldenia Rovi <124753028+Hellen240403@users.noreply.github.com>
2025-06-14 22:39:23 +0700

    Update home.py

--- home.py
+++ home.py
@@ -55,7 +55,7 @@ def app():
     with col1:
         st.markdown("""
         <div style="
-            background-color: #ffffff;
+            background-color: #f0f3fa;
             padding: 20px;
             border-radius: 12px;
             box-shadow: 2px 2px 10px rgba(0,0,0,0.1);

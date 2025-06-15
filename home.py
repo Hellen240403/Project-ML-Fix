6c5a0e0d0d732b91a96b12a3757627f67fca402b
Hellen Aldenia Rovi <124753028+Hellen240403@users.noreply.github.com>
2025-06-15 15:29:22 +0700

    Update home.py

--- home.py
+++ home.py
@@ -7,7 +7,7 @@ from PIL import Image
 #  CONFIG ACCUWEATHER API
 # ------------------------------------------------------------------ #
 # Pastikan ACCUWEATHER_KEY sudah ditambahkan ke .streamlit/secrets.toml
-API_KEY = "ISI_API_KEY_MU_DISINI"
+API_KEY = "xQGm4v0qhpHLusHsjGGA5O2GJPNXAQSO"
 LOCATION_KEY = "203449"  # Surabaya
 
 @st.cache_data(ttl=600)  # cache 10 menit

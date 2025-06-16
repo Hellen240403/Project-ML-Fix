3322634b6040e86830d9b9d9f97bc602d2e750fb
Hellen Aldenia Rovi <124753028+Hellen240403@users.noreply.github.com>
2025-06-16 10:29:32 +0700

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -77,7 +77,7 @@ def app():
         wind_speed = st.slider('🌬️ Kecepatan Angin (km/jam)', 0.0, 100.0, 25.0, 1.0)
         cloud_name = st.radio("☁️ Pilih Jenis Awan:", options=list(cloud_options.keys()), horizontal=True)
 
-        st.markdown(f"""
+    st.markdown(f"""
         <div style='text-align:center;'>
             <img src='{cloud_options[cloud_name]["img"]}' alt='Jenis Awan' style='max-width:300px; height:auto; border-radius:10px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);'>
             <p><strong>☁️ Jenis Awan: {cloud_name}</strong></p>
